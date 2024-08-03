f08ac9c4f8092a564a9fe93e97db946ddd0023c5
Coder-Jon014 <jon.coombs14@gmail.com>
2024-08-03 14:18:13 -0500

    Updates

--- dist/assets/ApplicationLogo-09B9ADW0.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-BPPzjWu5.js";function r(o){return t.jsx("img",{src:"/images/logo.png",alt:"logo",...o})}export{r as A};
---
+++ dist/assets/ApplicationLogo-Cju9_G4y.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-ekkVJCk3.js";function r(o){return t.jsx("img",{src:"/images/logo.png",alt:"logo",...o})}export{r as A};
